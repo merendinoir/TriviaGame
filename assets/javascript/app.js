bcf3869a7fc140000f2a414a8c24c04bf1eed17f
Isabella Merendino <isabellamerendino@Isabellas-MacBook-Pro.local>
2019-04-13 02:10:10 -0400

    Laid out some basic HTML/CSS

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+// user clicks start button to begin game - content is generated:
+    // timer appears and begins to countdown
+    // Question and appropriate choices appear
+
+// User clicks an answer choice - triggers a response to show whether the choice was correct or not;
